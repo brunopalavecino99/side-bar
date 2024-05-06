7141337a5630b5b907f4693adc81c4aa004fa005
Bruno Palavecino <bpalavecino@cormetalsa.local>
2024-05-06 14:35:53 -0300

    Delete App.jsx and add react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Header from './components/header/Header';
+/*import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/home/Home';
+import About from './pages/about/About';
+import Contact from './pages/contact/Contact';
+import Products from './pages/products/Products';*/
+
+const App = () => {
+  return (
+    <Header />
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
